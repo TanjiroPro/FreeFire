e752c4acaa6baee004469e45c278180a3c5c2bad
YALLICO OWNER <yallico2024@gmail.com>
2025-06-14 17:24:46 -0500

    Add files via upload

--- plugins/precio.js
+++ plugins/precio.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn }) => {
     let chat = global.db.data.chats[m.chat]
-    if (chat.setprecio) {
-        let precio = chat.setprecio;
-        await conn.reply(m.chat, precio, m);
+    if (chat.setprecios) {
+        let precios = chat.setprecios;
+        await conn.reply(m.chat, precios, m);
     } else {
-        m.reply(`> 𝙉𝙤 𝙨𝙚 𝙝𝙖 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙞𝙙𝙤 𝙪𝙣 𝙘𝙖𝙩á𝙡𝙤𝙜𝙤 𝙥𝙖𝙧𝙖 𝘾𝙤𝙢𝙥𝙖𝙧, 𝙪𝙩𝙞𝙡𝙞𝙯𝙖 .𝙨𝙚𝙩precio 𝙥𝙖𝙧𝙖 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙚𝙧 𝙪𝙣𝙤.🥖`);
+        m.reply(`> 𝙉𝙤 𝙨𝙚 𝙝𝙖 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙞𝙙𝙤 𝙪𝙣 𝙘𝙖𝙩á𝙡𝙤𝙜𝙤 𝙥𝙖𝙧𝙖 𝙋𝙧𝙤𝙢𝙤𝙨, 𝙪𝙩𝙞𝙡𝙞𝙯𝙖 .𝙨𝙚𝙩precios 𝙥𝙖𝙧𝙖 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙚𝙧 𝙪𝙣𝙤.🥖`);
     }
 }
-handler.command = ['precio'];
+handler.command = ['precios'];
 handler.group = true;
 export default handler;
